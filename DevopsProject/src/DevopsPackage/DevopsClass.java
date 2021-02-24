1246c6410ee5fdf2fd816b7fc0efe27bc0b2cd01
Gowthami Goli <gowthami.goli@mavenir.com:>
2021-02-24 11:11:55 +0530

    minor change

--- DevopsProject/src/DevopsPackage/DevopsClass.java
+++ DevopsProject/src/DevopsPackage/DevopsClass.java
@@ -3,9 +3,10 @@ package DevopsPackage;
 public class DevopsClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("Hello Devops");
 System.out.println("Hello Everyone");
+
 	}
 
 }
