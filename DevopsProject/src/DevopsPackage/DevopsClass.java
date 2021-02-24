1ed30251a0381ad3ffc88499db1d781cb4ee9b70
Gowthami Goli <gowthami.goli@mavenir.com:>
2021-02-24 11:09:09 +0530

    pusing the code

--- DevopsProject/src/DevopsPackage/DevopsClass.java
+++ DevopsProject/src/DevopsPackage/DevopsClass.java
@@ -5,6 +5,7 @@ public class DevopsClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Devops");
+System.out.println("Hello Everyone");
 	}
 
 }
