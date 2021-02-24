d7daaf22b8971b03df73cf9d2558f18af8f43ba1
Gowthami Goli <gowthami.goli@mavenir.com:>
2021-02-24 11:07:47 +0530

    My First commit

---
+++ DevopsProject/src/DevopsPackage/DevopsClass.java
@@ -0,0 +1,10 @@
+package DevopsPackage;
+
+public class DevopsClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Devops");
+	}
+
+}
